074e62729296de6ba2e6f3e33680ea423cc94652
poojaranawade <poojaranawade@gmail.com>
2017-11-23 14:13:23 -0800

    first commit

---
+++ myApp1.py
@@ -0,0 +1,27 @@
+import json
+from difflib import SequenceMatcher, get_close_matches
+
+data = json.load(open("data.json"))
+
+
+# print(data["rain"])
+
+def translate(entered_word):
+    # print(SequenceMatcher(None, "rainn", "rain").ratio())
+    entered_word = entered_word.lower()
+    if entered_word in data:
+        return data[entered_word]
+    else:
+        similar_word = get_close_matches(entered_word, data.keys())
+        if len(similar_word) > 0:
+            for i in similar_word:
+                decision = input("Do you mean: %s " % i)
+                if decision == 'y':
+                    return data[i]
+            return "Which word is that!"
+        else:
+            print("The word doesn't exist!")
+
+
+word = input("enter word: ")
+
